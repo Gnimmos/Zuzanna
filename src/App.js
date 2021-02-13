983835ce3966e60c3e34ecf2d82b383c570e584f
Gnimmos <gnicolaou92@gmail.com>
2021-02-13 19:34:26 +0200

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,41 @@ import './App.css';
 import ReactAudioPlayer from 'react-audio-player';
 import song from './song.mp3'
 import theend from './theend.jpg'
-import space from './space.jpg'
-import toopink from './toopink.jpg' 
+import spaceee from './spaceee.jpg'
+import pic5 from './pictures/pic5.jpg'
 import spacee from './spacee.jpg'
-import {  Row, Col, Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Media from 'react-bootstrap/Media';
+import spacegif from './spacegif.gif'
+import spacegif2 from './spacegif2.gif'
+import spacegif3 from './spacegif3.gif'
+import spacegif4 from './spacegif4.gif'
+import spacegif5 from './spacegif5.gif'
+import spacegif6 from './spacegif6.gif'
+import pic6 from './pictures/pic6.jpg'
+import spacegif8 from './spacegif8.gif'
+import pic7 from './pictures/pic7.jpg'
+import pic from './pictures/pic.jpg'
+import lastoone from './lastoone.gif'
+import lastoonepart2 from './lastoonepart2.gif'
+import lastonepart3 from './lastonepart3.gif'
+import lastlastoone from './lastlastoone.gif'
+import pic2 from './pictures/pic2.jpg'
+import pic3 from './pictures/pic3.jpg'
+import pic4 from './pictures/pic4.jpg'
+import pic8 from './pictures/pic8.jpg'
+import potw from './pictures/potw1838a.jpg'
+import something from './pictures/somethingsmart.jpg'
+import pic9 from './pictures/pic9.jpg'
+import pic10 from './pictures/pic10.png'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-            <Container fluid="lg">
+      <Container fluid="lg">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <div className="textup">
@@ -37,69 +62,196 @@ function App() {
          And 'cross the twinkling void the meteor whirls.
         `}</pre>
                 <hr></hr>
+                </div>
+      </div>
+                <Container fluid="lg">
+                <Row>
+                    <Col lg={12}>
+                               <iframe className="game" src="https://chromedino.com/batman/" frameborder="0" scrolling="no" width="100%" height="100%" loading="lazy"></iframe>
+                   </Col>
+              </Row>
 
         <br> 
         
         </br>
-      
-        </div>
-      </div>
-      <Container fluid="lg">
+        <Row  md={4}>
+                              
+                               <Col lg={6} className="   h-100 d-flex flex-column ">
+                                <Media className="moreleft2 mobile">
+                                  <Media.Body>
+                                  <a href="https://www.nasa.gov/feature/goddard/2020/hubble-sees-cosmic-flapping-bat-shadow" target="none">
 
-      <Row>
-        <Col md={12}>
-      <iframe className="dino"  src="https://chromedino.com/batman/"  frameborder="0" scrolling="no" width="100%" height="100%" loading="lazy"></iframe> 
-      </Col>   
-      </Row>
-      <Row className="positionthem" md={4} >      
-     
-                           
-                                  <Col md={4} >
-                                 <Media>
-                                      <Media.Body>
-                                      <img
-                                        width={300}
-                                        height={300}
-                                        className="ml-3"
-                                        src={toopink}
-                                        alt="Generic placeholder"
-                                      />
+                                   <img
+                                   width={500}
+                                  height={500}
+                                    className="mobile"
+                                    src={pic3}
+                                      alt="Generic placeholder"
+                                    />
+</a>
+<br/>                                                                      
+                                        <br/> 
+                                     </Media.Body>
+                              
+                                      </Media > 
+                                      </Col>
+                                      <Col lg={6} className="  h-100 d-flex flex-column ">
+                                     <Media className="moreright2 mobile">
+                                        <Media.Body>
+                                        <a href="https://www.nasa.gov/feature/goddard/2020/for-30th-anniversary-hubble-releases-images-of-30-celestial-gems" target="none">
+                                     <img
+                                      width={500}
+                                     height={500}
+                                     className="mobile"
+                                       src={pic5}
+                                       alt="Generic placeholder"
+                                        />
 
-                                        <h5>Manifesto</h5>
+                                        </a>
+                                        <br/>                                                                      
+                                        <br/>                                                                      
+                                         </Media.Body>
+                              
+                                         </Media> 
+                                         </Col>
+                              
+                                       <Col lg={6} className=" h-100 d-flex flex-column ">
+                                        <Media  className="moreleft mobile"> 
+                                          <Media.Body>
+                                          <a href="https://www.nasa.gov/image-feature/goddard/2021/hubble-takes-portrait-of-the-lost-galaxy" target="none">
+                                          <img
+                                          width={500}
+                                          height={500}
+                                         className="mobile"
+                                          src={pic9}
+                                          alt="Generic placeholder"
+                                         />
+                                     </a>
+                                         
+                                     <br/>                                                                      
+                                     <br/>                                                                      
 
-                                        <h5 className="names">VAGGELIS</h5>
-                                      
-                                      </Media.Body>
+                                          </Media.Body>
+                              
+                                          </Media> 
+                                         </Col>
 
-                                    </Media> 
-                                  </Col>
+                                         <Col lg={6} className=" h-100 d-flex flex-column ">
+                                        <Media className="moreright mobile">
+                                          <Media.Body>
+                                          <a href="https://www.nasa.gov/image-feature/goddard/2020/hubble-views-a-galactic-waterfall" target="none">
 
-                                  <Col md={4} >
-                                      <Media>
-                                      <Media.Body>
-                                      <img
-                                        width={300}
-                                        height={300}
-                                        className="ml-3"
-                                        src={spacee}
-                                        alt="Generic placeholder"
-                                      />
+                                          <img
+                                          width={500}
+                                          height={500}
+                                         className="mobile"
+                                          src={pic6}
+                                          alt="Generic placeholder"
+                                         />
+                                          </a>
+                                          <br/>                                                                      
+                                          <br/>                                                                      
 
-                                        <h5>Ola zen</h5>
-                                        <h5 className="names">KYRIACOS</h5>
+                                          </Media.Body>
+                              
+                                          </Media> 
+                                         </Col>
+                                         <Col lg={12}>
+                                         <iframe className="Georgio2" width="560" height="600" scrolling="no" src="http://cykod.github.io/AlienInvasion/?fbclid=IwAR0BH09mFft2u2Pnunvi-YDoQels020G85MHstZ-Lz9hBh0bx5nDTYyINGI" frameborder="0" allowfullscreen></iframe>
+                                         </Col>
+                                         <Col lg={6} className=" h-100 d-flex flex-column ">
+                                        <Media className="moreleft mobile">
+                                          <Media.Body>
+                                          <a href="https://www.nasa.gov/image-feature/goddard/2021/hubble-takes-portrait-of-the-lost-galaxy" target="none">
+                                          <img
+                                          width={500}
+                                          height={500}
+                                         className="mobile"
+                                          src={pic7}
+                                          alt="Generic placeholder"
+                                         />
+                                     </a>
+                                     <br/>                                                                      
+                                     <br/>                                                                      
 
-                                      </Media.Body>
+                                          </Media.Body>
+                              
+                                          </Media> 
+                                         </Col>
 
-                                    </Media>  
-                                  </Col>
-                             </Row>   
-                             </Container>
-      <div>
-        <img className="George" src={theend}></img>
+                                         <Col lg={6} className=" h-100 d-flex flex-column ">
+                                        <Media className="moreright mobile">
+                                          <Media.Body>
+                                            <a href="https://www.nasa.gov/feature/goddard/2020/quasar-tsunamis-rip-across-galaxies"target="none"> 
+                                          <img
+                                          width={500}
+                                          height={500}
+                                         className="mobile"
+                                          src={pic2}
+                                          alt="Generic placeholder"
+                                         />
+                                        </a>
+                                        <br/>                                                                      
+                                           <br/>
+                                          </Media.Body>
+                              
+                                          </Media> 
+                                         </Col>
+                                         <Col lg={6} className=" h-100 d-flex flex-column ">
+                                        <Media className="mobile">
+                                          <Media.Body>
+                                          <a href="https://www.nasa.gov/image-feature/eagle-nebula-s-pillars-of-creation-in-infrared" target="none">
+                                          <img
+                                                                                   className="mobile"
+
+                                          width={500}
+                                          height={500}
+                                          src={pic}
+                                          alt="Generic placeholder"
+                                         />
+                                     </a>
+                                     <br/>                                                                      
+                                     <br/>                                                                      
+
+                                          </Media.Body>
+                              
+                                          </Media> 
+                                         </Col>
+
+                                         <Col lg={6} className=" h-100 d-flex flex-column ">
+                                        <Media className="mobile">
+                                          <Media.Body>
+                                          <a href="https://www.nasa.gov/image-feature/goddard/2021/hubble-takes-portrait-of-the-lost-galaxy" target="none">
+                                          <img
+                                          width={500}
+                                          height={500}
+                                         className="mobile"
+                                          src={pic10}
+                                          alt="Generic placeholder"
+                                         />
+                                     </a>
+                                     <br/>                                                                      
+                                     <br/>                                                                      
+
+                                          </Media.Body>
+                              
+                                          </Media> 
+                                         </Col>
+
+                                       </Row>
+                                        </Container>
+
+                                       <div>
+                                     <img className="George" src={theend}></img>
+
+
+                                      </div>
+                                       <div> 
+
       </div>
       </Container>
-
-    </div>
+     
+  </div>
   );
 }
 
